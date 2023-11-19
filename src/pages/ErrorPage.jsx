80d63d0743bb1e770a9e19d2ae5d544c15af40b9
syedshaon <syedshaon99@gmail.com>
2023-11-19 11:22:24 +0600

    404 page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import OtherHeader from "../contents/OtherHeader";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function ErrorPage() {
-  return <div>error</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="teams container min-h-[800px] flex justify-center items-center px-2 md:px-7  ">
+        <div class="text-center">
+          <h1 class="mb-4 text-6xl font-semibold text-orange-600">404</h1>
+          <p class="mb-4 text-lg text-gray-600">Oops! Looks like you're lost.</p>
+          <div class="animate-bounce">
+            <svg class="mx-auto h-16 w-16 text-orange-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path>
+            </svg>
+          </div>
+          <p class="mt-4 text-gray-600">
+            Let's get you back{" "}
+            <a href="/" class="text-emerald-500">
+              home
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default ErrorPage;
